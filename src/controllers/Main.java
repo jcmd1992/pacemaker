ea6129164462f3776469f357ccb5f65375856535
20074582 <20074582@IT118PC18.wit.ie>
2017-09-29 14:51:23 +0100

    Third version of pacemaker-class with xstream

--- src/controllers/Main.java
+++ src/controllers/Main.java
@@ -1,14 +1,20 @@
 package controllers;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 import java.util.List;
+
+import com.thoughtworks.xstream.XStream;
+import com.thoughtworks.xstream.io.xml.DomDriver;
 
 import utils.FileLogger;
 import models.user;
 
 public class Main
 {
-	public static void main(String[] args)
+	 public static void main(String[] args) throws IOException
 	  {
 	    FileLogger logger = FileLogger.getLogger();
 	    logger.log("Creating user list");
@@ -18,6 +24,12 @@ public class Main
 	    users.add(new user("Homer", "Simpson", "bart@simpson.com", "secret"));
 	    users.add(new user("Lisa", "Simpson", "bart@simpson.com", "secret"));
 	    System.out.println(users);
+
+	    logger.log("Serializing contacts to XML");
+	    XStream xstream = new XStream(new DomDriver());
+	    ObjectOutputStream out = xstream.createObjectOutputStream(new FileWriter("users.xml"));
+	    out.writeObject(users);
+	    out.close();    
 
 	    logger.log("Finished - shutting down");
 	  }
