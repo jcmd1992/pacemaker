29a92b1e8df85500cecfa5a22a23ffc583f7bdda
20074582 <20074582@IT118PC18.wit.ie>
2017-09-29 14:38:23 +0100

    Introduced logger class

--- src/controllers/Main.java
+++ src/controllers/Main.java
@@ -3,16 +3,22 @@ package controllers;
 import java.util.ArrayList;
 import java.util.List;
 
+import utils.FileLogger;
 import models.user;
 
 public class Main
 {
-  public static void main(String[] args)
-  {
-    List<user> users = new ArrayList<user>();
-    users.add(new user("Bart", "Simpson", "bart@simpson.com", "secret"));
-    users.add(new user("Homer", "Simpson", "bart@simpson.com", "secret"));
-    users.add(new user("Lisa", "Simpson", "bart@simpson.com", "secret"));
-    System.out.println(users);
-  }
+	public static void main(String[] args)
+	  {
+	    FileLogger logger = FileLogger.getLogger();
+	    logger.log("Creating user list");
+
+	    List<user> users = new ArrayList<user>();
+	    users.add(new user("Bart", "Simpson", "bart@simpson.com", "secret"));
+	    users.add(new user("Homer", "Simpson", "bart@simpson.com", "secret"));
+	    users.add(new user("Lisa", "Simpson", "bart@simpson.com", "secret"));
+	    System.out.println(users);
+
+	    logger.log("Finished - shutting down");
+	  }
 }
